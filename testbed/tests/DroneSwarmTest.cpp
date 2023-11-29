6aea5cb4c98bbfdc284b6c97cc675c81e04c4a46
Joel Beedle <beedlejoel@gmail.com>
2023-11-29 23:30:14 +0000

    Removed redundant print statement

--- testbed/tests/DroneSwarmTest.cpp
+++ testbed/tests/DroneSwarmTest.cpp
@@ -6,17 +6,13 @@
 #include "imgui/imgui.h"
 #include "test.h"
 
-void createScreenBounds(b2World *world, float screenWidth, float screenHeight) {
+void createBounds(b2World *world, float screenWidth, float screenHeight) {
   // Define the ground body.
   b2BodyDef groundBodyDef;
   groundBodyDef.position.Set(0.0f, 0.0f);
 
-  // Call the body factory which allocates memory for the ground body
-  // from a pool and creates the ground box shape (also from a pool).
-  // The body is also added to the world.
   b2Body *groundBody = world->CreateBody(&groundBodyDef);
 
-  // Define the ground box shape.
   b2EdgeShape groundBox;
 
   // bottom
@@ -51,8 +47,8 @@ class DroneSwarmTest : public Test {
 
   DroneSwarmTest() {
     {
-      // Create Screen Bounds
-      createScreenBounds(m_world, screenWidth, screenHeight);
+      // Create Bounds
+      createBounds(m_world, screenWidth, screenHeight);
     }
     {
       // Initialise world and drones
@@ -64,10 +60,6 @@ class DroneSwarmTest : public Test {
       while (currentBody) {
         obstacles.push_back(currentBody);
         currentBody = currentBody->GetNext();
-      }
-      for (auto &obstacle : obstacles) {
-        printf("Obstacle: %f, %f\n", obstacle->GetPosition().x,
-               obstacle->GetPosition().y);
       }
       viewRange = 20.0f;
       separationDistance = 50.0f;
