417bbc258406a2832e047ad2bf297b752e8a702a
Joel Beedle <beedlejoel@gmail.com>
2023-11-30 17:56:01 +0000

    Improved DroneSwarmTest.cpp

--- testbed/tests/DroneSwarmTest.cpp
+++ testbed/tests/DroneSwarmTest.cpp
@@ -1,6 +1,12 @@
 #include <box2d/box2d.h>
 #include <stdio.h>
 
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <variant>
+#include <vector>
+
 #include "Drone.h"
 #include "FlockingBehaviour.h"
 #include "PheremoneBehaviour.h"
@@ -8,8 +14,47 @@
 #include "test.h"
 
 #define DRONE_COUNT 50
+#define BORDER_WIDTH 100.0f
+#define BORDER_HEIGHT 100.0f
 
-void createBounds(b2World *world, float screenWidth, float screenHeight) {
+// TODO: add numBehaviours at end to ensure it is updated throughout
+enum class BehaviourType { Flocking, Pheremone };
+
+struct BehaviourTypeInfo {
+  BehaviourType type;
+  std::string displayName;
+};
+
+class BehaviourFactory {
+ public:
+  using BehaviourParams = std::variant<FlockingParameters, PheremoneParameters>;
+
+  static SwarmBehaviour *createBehaviour(BehaviourType type,
+                                         BehaviourParams params) {
+    switch (type) {
+      case BehaviourType::Flocking:
+        return new FlockingBehaviour(
+            extractParameters<FlockingParameters>(params));
+      case BehaviourType::Pheremone:
+        return new PheremoneBehaviour(
+            extractParameters<PheremoneParameters>(params));
+      default:
+        throw std::runtime_error("Unknown behaviour type");
+    }
+  }
+
+ private:
+  template <typename T>
+  static T extractParameters(const BehaviourParams &params) {
+    if (auto p = std::get_if<T>(&params)) {
+      return *p;
+    } else {
+      throw std::runtime_error("Incorrect parameters for behaviour");
+    }
+  }
+};
+
+void createBounds(b2World *world) {
   // Define the ground body.
   b2BodyDef groundBodyDef;
   groundBodyDef.position.Set(0.0f, 0.0f);
@@ -19,103 +64,93 @@ void createBounds(b2World *world, float screenWidth, float screenHeight) {
   b2EdgeShape groundBox;
 
   // bottom
-  groundBox.SetTwoSided(b2Vec2(0.0f, 0.0f), b2Vec2(screenWidth, 0.0f));
+  groundBox.SetTwoSided(b2Vec2(0.0f, 0.0f), b2Vec2(BORDER_WIDTH, 0.0f));
   groundBody->CreateFixture(&groundBox, 0.0f);
 
   // top
-  groundBox.SetTwoSided(b2Vec2(0.0f, screenHeight),
-                        b2Vec2(screenWidth, screenHeight));
+  groundBox.SetTwoSided(b2Vec2(0.0f, BORDER_HEIGHT),
+                        b2Vec2(BORDER_WIDTH, BORDER_HEIGHT));
   groundBody->CreateFixture(&groundBox, 0.0f);
 
   // left
-  groundBox.SetTwoSided(b2Vec2(0.0f, 0.0f), b2Vec2(0.0f, screenHeight));
+  groundBox.SetTwoSided(b2Vec2(0.0f, 0.0f), b2Vec2(0.0f, BORDER_HEIGHT));
   groundBody->CreateFixture(&groundBox, 0.0f);
 
   // right
-  groundBox.SetTwoSided(b2Vec2(screenWidth, 0.0f),
-                        b2Vec2(screenWidth, screenHeight));
+  groundBox.SetTwoSided(b2Vec2(BORDER_WIDTH, 0.0f),
+                        b2Vec2(BORDER_WIDTH, BORDER_HEIGHT));
   groundBody->CreateFixture(&groundBox, 0.0f);
 }
 
 class DroneSwarmTest : public Test {
  public:
+  // Behaviours
   SwarmBehaviour *behaviour;
-  FlockingBehaviour *flockingBehaviour;
-  PheremoneBehaviour *pheremoneBehaviour;
+  BehaviourType currentBehaviourType;
 
+  // Parameters
+  FlockingParameters flockingParams = {50.0f, 1.0f, 1.0f, 1.0f};
+  PheremoneParameters pheremoneParams = {0.1f};
+
+  // Drone settings
   std::vector<Drone *> drones;
-  const float screenWidth = 100.0f;
-  const float screenHeight = 100.0f;
 
-  // Floats for Drones
-  float viewRange, maxSpeed, maxForce;
+  float viewRange = 20.0f;
+  float maxSpeed = 10.0f;
+  float maxForce = 0.3f;
 
-  // Floats for FlockingBehaviour
-  float separationDistance, alignmentWeight, cohesionWeight, separationWeight,
-      obstacleAvoidanceWeight;
+  // ImGUI settings to switch behaviours
+  int currentBehaviourIndex;  // Index of the currently selected behaviour
 
-  // Floats for PheremoneBehaviour
-  float decayRate;
-
-  // Imgui
+  // IMPORTANT: UPDATE WHEN ADDING BEHAVIOURS
+  std::vector<BehaviourType> indexToBehaviourType = {
+      BehaviourType::Flocking,  // 0
+      BehaviourType::Pheremone  // 1
+  };
+  const std::vector<BehaviourTypeInfo> behaviourTypes = {
+      {BehaviourType::Flocking, "flockingBehaviour"},
+      {BehaviourType::Pheremone, "pheremoneBehaviour"}};
   const char *behaviours[2] = {"flockingBehaviour", "pheremoneBehaviour"};
-  int currentBehaviour;  // Index of the currently selected behaviour
 
+ public:
   DroneSwarmTest() {
     {
-      // Create Bounds
-      createBounds(m_world, screenWidth, screenHeight);
-    }
-    {
+      initWorld();
+      initDefaultParameters();
+
       // Set initial behaviour
-      currentBehaviour = 0;  // flockingBehaviour TODO: enum?
-      // Initialise world and drones
-      b2Vec2 gravity(0.0f, 0.0f);
-      m_world->SetGravity(gravity);
+      currentBehaviourType = BehaviourType::Flocking;
+      currentBehaviourIndex = 0;
 
-      std::vector<b2Body *> obstacles;
-      b2Body *currentBody = m_world->GetBodyList();
-      while (currentBody) {
-        obstacles.push_back(currentBody);
-        currentBody = currentBody->GetNext();
-      }
-      viewRange = 20.0f;
-      separationDistance = 50.0f;
-      alignmentWeight = 1.0f;
-      cohesionWeight = 1.0f;
-      separationWeight = 1.0f;
-      obstacleAvoidanceWeight = 1.0f;
-      maxSpeed = 10.0f;
-      maxForce = 0.3f;
-
-      flockingBehaviour = new FlockingBehaviour(
-          separationDistance, alignmentWeight, cohesionWeight, separationWeight,
-          obstacleAvoidanceWeight);
-
-      decayRate = 0.1f;
-      pheremoneBehaviour = new PheremoneBehaviour(decayRate);
-
-      for (int i = 0; i < DRONE_COUNT; i++) {
-        drones.push_back(
-            new Drone(m_world,
-                      b2Vec2(rand() % static_cast<int>(screenWidth),
-                             rand() % static_cast<int>(screenHeight)),
-                      getSelectedBehaviour(currentBehaviour)));
-      }
+      // Set behaviour to equal our current behaviour
+      UpdateBehaviour();
+      // Create our drones with our selected behaviour
+      createDrones(behaviour);
     }
   }
 
-  void CreateDrones() {
-    flockingBehaviour = new FlockingBehaviour(
-        separationDistance, alignmentWeight, cohesionWeight, separationWeight,
-        obstacleAvoidanceWeight);
+  void initWorld() {
+    createBounds(m_world);
+    b2Vec2 gravity(0.0f, 0.0f);
+    m_world->SetGravity(gravity);
+  }
 
+  void initDefaultParameters() {
+    viewRange = 20.0f;
+    maxSpeed = 10.0f;
+    maxForce = 0.3f;
+
+    flockingParams = {50.0f, 1.0f, 1.0f, 1.0f};
+    pheremoneParams = {0.1f};
+  }
+
+  void createDrones(SwarmBehaviour *b) {
     for (int i = 0; i < DRONE_COUNT; i++) {
       drones.push_back(
           new Drone(m_world,
-                    b2Vec2(rand() % static_cast<int>(screenWidth),
-                           rand() % static_cast<int>(screenHeight)),
-                    flockingBehaviour, viewRange, maxSpeed, maxForce));
+                    b2Vec2(rand() % static_cast<int>(BORDER_WIDTH),
+                           rand() % static_cast<int>(BORDER_HEIGHT)),
+                    behaviour));
     }
   }
 
@@ -126,24 +161,9 @@ class DroneSwarmTest : public Test {
     drones.clear();
   }
 
-  SwarmBehaviour *getSelectedBehaviour(int behaviourIndex) {
-    switch (behaviourIndex) {
-      case (0):
-        // FlockingBehaviour
-        return flockingBehaviour;
-      case (1):
-        // PheremoneBehaviour
-        return pheremoneBehaviour;
-      default:
-        return nullptr;
-    }
-    return nullptr;
-  }
-
-  void SetBehaviour(int behaviourIndex) {
-    SwarmBehaviour *newBehaviour = getSelectedBehaviour(behaviourIndex);
+  void SetBehaviour() {
     for (auto &drone : drones) {
-      drone->setBehaviour(newBehaviour);
+      drone->setBehaviour(behaviour);
     }
   }
 
@@ -156,24 +176,20 @@ class DroneSwarmTest : public Test {
     }
   }
 
-  void UpdateBehaviourSettings(int behaviourIndex) {
+  void UpdateBehaviour() {
+    currentBehaviourType = behaviourTypes[currentBehaviourIndex].type;
     SwarmBehaviour *newBehaviour;
-    switch (behaviourIndex) {
-      case (0):
-        // FlockingBehaviour
-        newBehaviour = new FlockingBehaviour(
-            separationDistance, alignmentWeight, cohesionWeight,
-            separationWeight, obstacleAvoidanceWeight);
+    switch (currentBehaviourType) {
+      case (BehaviourType::Flocking):
+        newBehaviour = BehaviourFactory::createBehaviour(
+            BehaviourType::Flocking, flockingParams);
         break;
-      case (1):
-        // PheremoneBehaviour
-        newBehaviour = new PheremoneBehaviour(decayRate);
+      case (BehaviourType::Pheremone):
+        newBehaviour = BehaviourFactory::createBehaviour(
+            BehaviourType::Pheremone, pheremoneParams);
         break;
     }
-    // Update the drones with the new behaviour
-    for (auto &drone : drones) {
-      drone->setBehaviour(newBehaviour);
-    }
+    behaviour = newBehaviour;
   }
 
   void UpdateUI() override {
@@ -183,9 +199,23 @@ class DroneSwarmTest : public Test {
                  ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize);
 
     // Dropdown to select the behaviour
-    if (ImGui::Combo("Behaviour", &currentBehaviour, behaviours,
-                     IM_ARRAYSIZE(behaviours))) {
-      SetBehaviour(currentBehaviour);
+    if (ImGui::BeginCombo(
+            "Behaviour",
+            behaviourTypes[currentBehaviourIndex].displayName.c_str())) {
+      for (int i = 0; i < behaviourTypes.size(); i++) {
+        bool isSelected = (currentBehaviourIndex == i);
+        if (ImGui::Selectable(behaviourTypes[i].displayName.c_str(),
+                              isSelected)) {
+          currentBehaviourIndex = i;
+          UpdateBehaviour();  // Assuming this function handles the logic for
+                              // updating behaviour
+          SetBehaviour();  // Assuming this function applies the new behaviour
+        }
+        if (isSelected) {
+          ImGui::SetItemDefaultFocus();
+        }
+      }
+      ImGui::EndCombo();
     }
 
     // Drone settings
@@ -202,35 +232,41 @@ class DroneSwarmTest : public Test {
     // Display different behaviour settings depending on the behaviour selected
     ImGui::Text("Behaviour Settings");
     bool distance, alignment, cohesion, separation, avoidance, decay;
-    switch (currentBehaviour) {
+    switch (currentBehaviourIndex) {
       case 0:
         // FlockingBehaviour
-        distance = ImGui::SliderFloat("separationDistance", &separationDistance,
-                                      0.0f, 100.0f);
-        alignment =
-            ImGui::SliderFloat("alignmentWeight", &alignmentWeight, 0.0f, 2.0f);
-        cohesion =
-            ImGui::SliderFloat("cohesionWeight", &cohesionWeight, 0.0f, 2.0f);
-        separation = ImGui::SliderFloat("separationWeight", &separationWeight,
-                                        0.0f, 2.0f);
+        distance = ImGui::SliderFloat("separationDistance",
+                                      &flockingParams.separationDistance, 0.0f,
+                                      100.0f);
+        alignment = ImGui::SliderFloat(
+            "alignmentWeight", &flockingParams.alignmentWeight, 0.0f, 2.0f);
+        cohesion = ImGui::SliderFloat(
+            "cohesionWeight", &flockingParams.cohesionWeight, 0.0f, 2.0f);
+        separation = ImGui::SliderFloat(
+            "separationWeight", &flockingParams.separationWeight, 0.0f, 2.0f);
         avoidance = ImGui::SliderFloat("obstacleAvoidanceWeight",
-                                       &obstacleAvoidanceWeight, 0.0f, 2.0f);
+                                       &flockingParams.obstacleAvoidanceWeight,
+                                       0.0f, 2.0f);
         if (distance || alignment || cohesion || separation || avoidance) {
-          UpdateBehaviourSettings(currentBehaviour);
+          UpdateBehaviour();
+          SetBehaviour();
         }
         break;
       case 1:
         // Pheremone Behaviour
-        decay = ImGui::SliderFloat("decayRate", &decayRate, 0.0f, 1.0f);
+        decay = ImGui::SliderFloat("decayRate", &pheremoneParams.decayRate,
+                                   0.0f, 1.0f);
         if (decay) {
-          UpdateBehaviourSettings(currentBehaviour);
+          UpdateBehaviour();
+          SetBehaviour();
         }
         break;
     }
 
     if (ImGui::Button("Reset Simulation")) {
       DestroyDrones();
-      CreateDrones();
+      initDefaultParameters();
+      createDrones(behaviour);
     }
 
     ImGui::End();
