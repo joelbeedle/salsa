24796ea11399388e2fda6e74ce3d571e6f4f3bf0
Joel Beedle <68159518+joelbeedle@users.noreply.github.com>
2024-06-07 13:09:35 +0100

    Update tree.cpp

--- src/tree.cpp
+++ src/tree.cpp
@@ -32,6 +32,6 @@ Tree::Tree(b2World *world, int treeID, const b2Vec2 &position, bool diseased,
 
   // Set diseased status of dree
   float infectionChance = 0.05f;
-  float randomValue = static_cast<float>(rand()) / static_cast<float>(std::RAND_MAX);
+  float randomValue = static_cast<float>(std::rand()) / static_cast<float>(std::RAND_MAX));
   this->diseased = randomValue < infectionChance ? true : diseased;
 }
