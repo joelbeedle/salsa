53b018eb138b684c576f6825d700c384de46a1cf
Joel Beedle <68159518+joelbeedle@users.noreply.github.com>
2024-06-24 16:40:33 +0100

    Update mock_behaviour.h

--- test/mock_behaviour.h
+++ test/mock_behaviour.h
@@ -10,9 +10,6 @@ class MockBehaviour : public swarm::Behaviour {
               (const std::vector<std::unique_ptr<swarm::Drone>> &drones,
                swarm::Drone &currentDrone),
               (override));
-
-  MOCK_METHOD((std::unordered_map<std::string, swarm::behaviour::Parameter *>),
-              getParameters, (), (override));
   MOCK_METHOD(void, clean,
               (const std::vector<std::unique_ptr<swarm::Drone>> &drones),
               (override));
