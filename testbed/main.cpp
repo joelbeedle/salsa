23f104f15659e43868e2453b72925dcb77a47031
Joel Beedle <68159518+joelbeedle@users.noreply.github.com>
2024-07-03 20:34:30 +0100

    Add queue selection to headless mode

--- testbed/main.cpp
+++ testbed/main.cpp
@@ -14,10 +14,14 @@ int main(int argc, char** argv) {
   bool headless = false;
   bool verbose = false;
   bool no_plots = false;
+  std::string queue_path = "none";
 
   app.add_flag("--headless", headless, "Run in headless mode");
   app.add_flag("-v,--verbose", verbose, "Verbose output")->needs("--headless");
-  app.add_flag("--no-plots", no_plots, "Do not plot the results");
+  app.add_flag("--no-plots", no_plots, "Do not plot the results")
+      ->needs("--headless");
+  app.add_option("-q,--queue", queue_path, "Path to the queue file")
+      ->needs("--headless");
   CLI11_PARSE(app, argc, argv);
 
   auto testbed_console = spdlog::stdout_color_mt("testbed_console");
@@ -28,6 +32,16 @@ int main(int argc, char** argv) {
   if (headless) {
     // Run in headless mode
     testbed::user();
+
+    if (queue_path != "none") {
+      std::cout << "Loading queue from " << queue_path << std::endl;
+      if (salsa::TestQueue::load(queue_path)) {
+        std::cout << "Queue loaded successfully" << std::endl;
+      } else {
+        std::cout << "Failed to load queue" << std::endl;
+        return 1;
+      }
+    }
     testbed_console->set_level(spdlog::level::warn);
     if (!no_plots) {
       testbed::plot_drone_distances = true;
